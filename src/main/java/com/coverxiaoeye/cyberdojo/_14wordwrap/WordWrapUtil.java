6697e8ba0f122ddfe70bd1ffe9a2d0192b094c2b
coverxiaoeye <cover_eye@163.com>
2016-09-14 17:20:05 +0800

    Update and rename BreakLine.java to WordWrapUtil.java

--- src/main/java/com/coverxiaoeye/cyberdojo/_14wordwrap/WordWrapUtil.java
+++ src/main/java/com/coverxiaoeye/cyberdojo/_14wordwrap/WordWrapUtil.java
@@ -1,12 +1,12 @@
-public class BreakLine{
+public class WordWrapUtil{
 
-    public static String breakLine(String str,int width) {
+    public static String wordwrap(String str,int width) {
         if(str.length()<=width) return str;
         int lastSpaceIdx = str.lastIndexOf(" ",width);
         if(lastSpaceIdx<0)
             return  str.substring(0,width)+"\n"
-                        +breakLine(str.substring(width),width);
+                        + wordwrap(str.substring(width),width);
         return str.substring(0,lastSpaceIdx)+"\n" 
-                        +breakLine(str.substring(lastSpaceIdx+1),width);
+                        + wordwrap(str.substring(lastSpaceIdx+1),width);
     }
 }
