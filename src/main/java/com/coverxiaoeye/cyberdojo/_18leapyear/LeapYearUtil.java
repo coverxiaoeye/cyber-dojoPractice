cf8ab138d060024904a8316fd25dc29817cbbc0a
coverxiaoeye <cover_eye@163.com>
2016-10-29 18:01:46 +0800

    consolidate conditional expression

--- src/main/java/com/coverxiaoeye/cyberdojo/_18leapyear/LeapYearUtil.java
+++ src/main/java/com/coverxiaoeye/cyberdojo/_18leapyear/LeapYearUtil.java
@@ -3,10 +3,8 @@ package com.coverxiaoeye.cyberdojo._18leapyear;
 public class LeapYearUtil{
  
     public static boolean isLeapYear(int year) {
-        if(year%400==0){
-            return true;
-        }
-        if(year%4==0 && year%100!=0){
+        if(year%400==0
+            ||(year%4==0 && year%100!=0)){
             return true;
         }
         return false;
