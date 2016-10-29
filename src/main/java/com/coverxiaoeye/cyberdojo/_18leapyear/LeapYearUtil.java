8c45ceea906819ba4ebd3a8a49d805c3f55008d7
coverxiaoeye <cover_eye@163.com>
2016-10-29 17:59:40 +0800

    refactoring nested if

--- src/main/java/com/coverxiaoeye/cyberdojo/_18leapyear/LeapYearUtil.java
+++ src/main/java/com/coverxiaoeye/cyberdojo/_18leapyear/LeapYearUtil.java
@@ -1,13 +1,13 @@
 package com.coverxiaoeye.cyberdojo._18leapyear;
 
 public class LeapYearUtil{
-
+ 
     public static boolean isLeapYear(int year) {
+        if(year%400==0){
+            return true;
+        }
         if(year%4==0){
             if(year%100==0){
-                if(year%400==0){
-                    return true;
-                }
                 return false;
             }
             return true;
