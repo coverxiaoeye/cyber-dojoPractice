3291ddd6e87493560c4935f05b41a75ba1f21068
coverxiaoeye <cover_eye@163.com>
2016-11-02 18:00:27 +0800

    add escape character and character set support

--- src/main/java/com/coverxiaoeye/cyberdojo/_17regularexpression/RegExpParser.java
+++ src/main/java/com/coverxiaoeye/cyberdojo/_17regularexpression/RegExpParser.java
@@ -8,12 +8,12 @@ public class RegExpParser{
         int end =0;
         String matchedStr =null;
         for(int i=0;i<input.length();i++){
-           matchedStr = findMatch(regex,input.substring(i));
-           if(matchedStr != null){
+            matchedStr = findMatch(regex,input.substring(i));
+            if(matchedStr != null&&!matchedStr.equals("")){
                 start=i;
                 end = start + matchedStr.length();
                 break;
-           }
+            }
         }
         return printResult(input,start,end);
     }
@@ -21,39 +21,60 @@ public class RegExpParser{
     private static String findMatch(String regex,String str){
         String matched = null;
         if(regex.equals("")) return "";
+
+        int nextStrCharIndex = 1;
+        int curRegCharIndex = 0;
+        int nextRegCharIndex = 1;
+        boolean escaped = false;
+        if (regex.charAt(0)=='\\'){
+            curRegCharIndex =1;
+            nextRegCharIndex =2;
+            escaped =true;
+        }
+        boolean inCharacterSet = false;
+        if (regex.charAt(0)=='['){
+            char min = regex.charAt(1);
+            char max = regex.charAt(3);
+            nextRegCharIndex = 5;
+            if(str.charAt(0)>=min&&str.charAt(0)<=max){
+                inCharacterSet = true;
+            }
+        }
+
         if(str.equals("")) {
-            if(regex.length()>1&&regex.charAt(1)=='*') return "";
+            if(regex.length()>nextRegCharIndex&&regex.charAt(nextRegCharIndex)=='*') return "";
             return null;
         }
-        if(str.charAt(0)==regex.charAt(0)||regex.charAt(0)=='.'){
-            if(regex.length()>1&&regex.charAt(1)=='+'){
+
+        if(inCharacterSet||str.charAt(0)==regex.charAt(curRegCharIndex)||(escaped ==false && regex.charAt(curRegCharIndex)=='.')){
+            if(regex.length()>nextRegCharIndex&&regex.charAt(nextRegCharIndex)=='+'){
                 String subMatched = findMatch(regex,str.substring(1));
                 if(subMatched==null){
-                    subMatched = findMatch(regex.substring(2),str.substring(1));
+                    subMatched = findMatch(regex.substring(nextRegCharIndex+1),str.substring(1));
                 }
                 if(subMatched!=null){
                     matched = str.charAt(0) + subMatched;
                 }
-            }else if(regex.length()>1&&regex.charAt(1)=='*'){
+            }else if(regex.length()>nextRegCharIndex&&regex.charAt(nextRegCharIndex)=='*'){
                 String subMatched = findMatch(regex,str.substring(1));
                 if(subMatched==null){
-                    subMatched = findMatch(regex.substring(2),str.substring(1));
+                    subMatched = findMatch(regex.substring(nextRegCharIndex+1),str.substring(1));
                 }
                 if(subMatched==null){
-                    subMatched = findMatch(regex.substring(2),str);
+                    subMatched = findMatch(regex.substring(nextRegCharIndex+1),str);
                 }
                 if(subMatched!=null){
                     matched = str.charAt(0) + subMatched;
                 }
             }else{
-                String subMatched = findMatch(regex.substring(1),str.substring(1));
+                String subMatched = findMatch(regex.substring(nextRegCharIndex),str.substring(1));
                 if(subMatched!=null){
                     matched = str.charAt(0) + subMatched;
                 }
             }
         }else{
-            if(regex.length()>1&&regex.charAt(1)=='*'){
-                matched = findMatch(regex.substring(2),str);
+            if(regex.length()>nextRegCharIndex&&regex.charAt(nextRegCharIndex)=='*'){
+                matched = findMatch(regex.substring(nextRegCharIndex+1),str);
             }
         }
         return matched;
