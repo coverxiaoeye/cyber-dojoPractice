c267b4a6b75b6e6ec7814e7c6af8dd33534186cc
coverxiaoeye <cover_eye@163.com>
2017-06-05 19:50:35 +0800

    Update CountCoins.java

--- src/main/java/com/coverxiaoeye/cyberdojo/_09countcoins/CountCoins.java
+++ src/main/java/com/coverxiaoeye/cyberdojo/_09countcoins/CountCoins.java
@@ -1,13 +1,59 @@
 package com.coverxiaoeye.cyberdojo._09countcoins;
 
+import java.util.*;
+
 public class CountCoins {
 
     public static int[] coinValues = {1, 5, 10, 25};
     public static int countMakeChangeWays(int numInCent) {
         if (numInCent<=0) 
             throw new IllegalArgumentException("numInCent should be great than 0 ");
-        return countWaysWhenSetCeiling(numInCent,numInCent);
+        //return countWaysWhenSetCeiling(numInCent,numInCent);
+//        List<Integer> initList = new ArrayList<>();
+//        Collections.addAll(initList,25,10,5,1);
+//        return countWays(numInCent, initList);
+        return countWaysArr(numInCent, 3);
     }
+
+    private static int countWaysArr(int numInCent,int coinIndex){
+        int maxCoinIndex = getMaxCoinIndex(numInCent);
+        maxCoinIndex= maxCoinIndex<coinIndex?maxCoinIndex:coinIndex;
+        if (maxCoinIndex ==0) return 1;
+
+        int includeways = 0;
+        int excludeways = 0;
+
+        if (numInCent == coinValues[maxCoinIndex]) includeways = 1;
+        else {
+            includeways = countWaysArr(numInCent - coinValues[maxCoinIndex], maxCoinIndex);
+        }
+        excludeways = countWaysArr(numInCent, maxCoinIndex-1);
+        return includeways+excludeways;
+    }
+
+    //coinTypee降序排列
+    private static int countWays(int numInCent,List<Integer> coinTypes){
+        for (Iterator<Integer> iterator = coinTypes.iterator(); iterator.hasNext(); ) {
+            Integer next =  iterator.next();
+            if(next>numInCent) iterator.remove();
+        }
+        if (coinTypes.size() == 1) return 1;
+
+        int includeways = 0;
+        int excludeways = 0;
+
+        List<Integer> includeThisList = new ArrayList<Integer>(coinTypes);
+        List<Integer> excludeThisList = new ArrayList<Integer>(coinTypes);
+        excludeThisList.remove(0);
+
+        if (numInCent == coinTypes.get(0)) includeways = 1;
+        else {
+            includeways = countWays(numInCent-coinTypes.get(0),includeThisList);
+        }
+        excludeways = countWays(numInCent,excludeThisList);
+        return includeways+excludeways;
+    }
+
     
     //count different ways when set the max coin value(e.g. 10cents),
     //the first coin from high value to low, 25cents->10cents->5cents->1cent
@@ -33,5 +79,8 @@ public class CountCoins {
             }
         }
         return 0;
+    }
+    public static void main(String[] args){
+        System.out.println(countMakeChangeWays(100));
     }
 }
